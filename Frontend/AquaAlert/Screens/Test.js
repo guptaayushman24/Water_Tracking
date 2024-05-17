436cd412e76ca53ee5a7a73362dff11d1d845add
Ayushman Gupta <guptaayushman24@gmail.com>
2024-05-18 00:32:43 +0530

    Alert Box is created

--- Frontend/AquaAlert/Screens/Test.js
+++ Frontend/AquaAlert/Screens/Test.js
@@ -2,6 +2,7 @@ import { Text,View ,StyleSheet, TouchableOpacity} from "react-native";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { TextInput } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
 // const Detail= async()=>{
 //     try{
 //        const detail =  await axios.get('http://10.0.2.2:5000/signup/usersignupdetail');
@@ -13,10 +14,12 @@ import { TextInput } from "react-native-paper";
 //     }
 // }
 const Test=()=>{
+
 const [text, setText] = useState("");
 
 const [matchfound,setmatchfound] = useState(false);
 const [storedata,setstoredata] = useState([]);
+const colors = ['#db425b', '#2893f3'];
 useEffect(()=>{
     const Detail= async()=>{
         try{
@@ -59,7 +62,18 @@ return(
                     <Text>Match not Found</Text>
                 )
             }
-            </View>
+
+
+            <LinearGradient style={styles.box} colors={colors}>
+                <View style={styles.alertbox}>
+                    <Text style={styles.alerttxt}>User Alerady Registered !!!!</Text>
+
+                    <View style={styles.okbtn}>
+                    <TouchableOpacity><Text style={styles.oktxt}>OK</Text></TouchableOpacity>
+                </View>
+                </View>
+            </LinearGradient>
+             </View>
     )
 }
 
@@ -74,5 +88,26 @@ const styles = StyleSheet.create({
         alignItems:"center",
 
         marginTop:80,
+    },
+    box:{
+        alignItems:"center"
+    },
+    alertbox:{
+       width:350,
+       height:125,
+    //    backgroundColor:'red',
+    },
+    alerttxt:{
+        marginLeft:60,
+        marginTop:35,
+        fontSize:20
+    },
+    okbtn:{
+        // backgroundColor:"green",
+        marginLeft:280,
+        marginTop:38,
+    },
+    oktxt:{
+        textAlign:"center"
     }
 })
