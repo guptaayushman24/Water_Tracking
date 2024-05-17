1e64f4b8f2b531065c43d926a66a5cebca6489e4
Ayushman Gupta <guptaayushman24@gmail.com>
2024-05-17 17:18:36 +0530

    Email Matching Done

--- Frontend/AquaAlert/App.js
+++ Frontend/AquaAlert/App.js
@@ -1,16 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Signup from './Screens/Signup';
 import Signin from './Screens/Signin';
+import Test from './Screens/Test';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <Signup>
+    // <Signup>
 
-    </Signup>
+    // </Signup>
 
     // <Signin></Signin>
+    <Test></Test>
   );
 
 }
